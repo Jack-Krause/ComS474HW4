07a3eba834de55fef56bba9dc49d7dea3c01b9ea
Jack Krause <jmkrause@iastate.edu>
2025-05-02 12:39:30 -0500

    regex parsed out data better

---
+++ analysis.py
@@ -0,0 +1,36 @@
+import os
+import re
+import glob
+
+
+cwd = os.getcwd()
+print(cwd)
+
+log_files = glob.glob(os.path.join(cwd, "*.log"))
+if not log_files:
+    raise FileNotFoundError("log files not found.")
+
+write_dir = os.path.join(cwd, 'parsed')
+if not os.path.exists(write_dir):
+    os.mkdir(write_dir)
+
+re_pattern = r"\d{1,2}\.\d{1,3}\%"
+
+for f in log_files:
+    print(f)
+    contents_str = ""
+    with open(f, 'r') as open_file:
+        for line in open_file:
+            match = re.search(re_pattern, line)
+            print(f"match:\n{match}")
+            
+            if match:
+                contents_str += match.group(0) + "\n"
+                
+    basename = os.path.basename(f)
+    with open(os.path.join(write_dir, basename), 'w') as write_file:
+        write_file.write(contents_str)
+
+
+
+
